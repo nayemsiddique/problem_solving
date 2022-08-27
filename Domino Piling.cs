aea7bc62cbe8a31007674990c0f31f068ba3e558
nayemsiddique <nayemsiddiqueasif@gmail.com>
2022-08-28 00:03:36 +0600

    A.Domino piling

---
+++ Domino Piling.cs
@@ -0,0 +1,12 @@
+﻿
+string xy = Console.ReadLine()??"0 0";
+
+int x = Convert.ToInt32(xy.Split(" ")[0]);
+int y = Convert.ToInt32(xy.Split(" ")[1]);
+
+
+
+int result = (x * y) / 2;
+
+
+Console.WriteLine(result);
